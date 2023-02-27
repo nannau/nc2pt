15f33195f1c2ff8e49d881d164c8c3ad2b9aa39a
Nic <nicannau@gmail.com>
2023-02-27 14:44:16 -0800

    Updates package name: ClimateXPrep

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-   name='climatex_prep',
+   name='ClimateXPrep',
    version='0.1.0',
    author='Nic Annau',
    author_email='nicannau@gmail.com ',
@@ -13,6 +13,6 @@ setup(
          "numpy",
          "matplotlib",
          "scipy",
-         "xarray"
+         "xarray",
          ],
 )
