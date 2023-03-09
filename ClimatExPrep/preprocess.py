15060087ad9753581382ac2100abdf3990cf544d
Nic <nicannau@gmail.com>
2023-03-08 19:37:03 -0800

    Back to zarr

--- ClimatExPrep/preprocess.py
+++ ClimatExPrep/preprocess.py
@@ -18,6 +18,15 @@ from ClimatExPrep.preprocess_helpers import (
     compute_standardization,
     write_to_zarr
 )
+import torch
+
+
+def make_batches(ds, steps_per_file, randomize):
+    return (
+        torch.split(torch.randperm(ds.time.size), steps_per_file)
+        if randomize
+        else torch.split(torch.arange(ds.time.size), steps_per_file)
+    )
 
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
@@ -81,8 +90,8 @@ def main(cfg) -> None:
 
         # Train test split
         logging.info("Splitting dataset...")
-        test_lr, train_lr = train_test_split(lr, cfg.time.test_years)
-        test_hr, train_hr = train_test_split(hr, cfg.time.test_years)
+        train_lr, test_lr = train_test_split(lr, cfg.time.test_years)
+        train_hr, test_hr = train_test_split(hr, cfg.time.test_years)
 
         # Standardize the dataset.
         logging.info("Standardizing dataset...")
@@ -93,11 +102,19 @@ def main(cfg) -> None:
 
         # Write the output to disk.
         logging.info("Writing test output...")
-        write_to_zarr(test_lr, f"{cfg.lr.output_path}/test_lr.zarr")
-        write_to_zarr(test_hr, f"{cfg.hr.output_path}/test_hr.zarr")
+        # batches = make_batches(test_lr, cfg.steps_per_file, cfg.randomize)
+        # write_to_pt(test_lr, f"{cfg.lr.output_path}/test/", batches)
+        # write_to_pt(test_hr, f"{cfg.hr.output_path}/test/", batches)
+        # logging.info("Writing train output...")
+        # batches = make_batches(train_lr, cfg.steps_per_file, cfg.randomize)
+        # write_to_pt(train_lr, f"{cfg.lr.output_path}/train/", batches)
+        # write_to_pt(train_hr, f"{cfg.hr.output_path}/train/", batches)
+
+        write_to_zarr(test_lr, f"{cfg.lr.output_path}/test_lr")
+        write_to_zarr(test_hr, f"{cfg.hr.output_path}/test_hr")
         logging.info("Writing train output...")
-        write_to_zarr(train_lr, f"{cfg.lr.output_path}/train_lr.zarr")
-        write_to_zarr(train_hr, f"{cfg.hr.output_path}/train_hr.zarr")
+        write_to_zarr(train_lr, f"{cfg.lr.output_path}/train_lr")
+        write_to_zarr(train_hr, f"{cfg.hr.output_path}/train_hr")
 
         end = timer()
         logging.info("Done!")
