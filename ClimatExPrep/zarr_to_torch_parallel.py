07d5fbcb92d3de502174df2b1c0a45ba916e27e5
Nic Annau <nicannau@gmail.com>
2023-04-06 09:56:58 -0700

    Manual NaN check

--- ClimatExPrep/zarr_to_torch_parallel.py
+++ ClimatExPrep/zarr_to_torch_parallel.py
@@ -15,6 +15,7 @@ from tqdm.contrib.concurrent import process_map
 def parallel_loop(i, path, arr):
     arr = arr.values
     x = torch.tensor(np.array(arr))
+    assert not torch.isnan(x).any(), f"NaNs found in {s} {res} {var} {i}"
     torch.save(x, path)
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
