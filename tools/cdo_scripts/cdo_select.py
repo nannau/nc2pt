b3edbd36799d721d20cb6159de86ed13a855a312
Nic Annau <nicannau@gmail.com>
2023-10-13 11:57:03 -0700

    Adds cdo scripts as python files to tools -- config

--- tools/cdo_scripts/cdo_select.py
+++ tools/cdo_scripts/cdo_select.py
@@ -4,25 +4,34 @@ import subprocess
 import shutil
 import logging
 
-@hydra.main(config_name="config")
+
+@hydra.main(config_name="cdo_config.yaml", config_path=".")
 def main(cfg):
     # Initialize logging
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
     # Check if the 'cdo' command is available
     if shutil.which("cdo") is None:
-        raise EnvironmentError("CDO (Climate Data Operators) is not installed or not in your system's PATH. Please install CDO and ensure it's accessible.")
+        raise EnvironmentError(
+            "CDO (Climate Data Operators) is not installed or not in your system's PATH. Please install CDO and ensure it's accessible."
+        )
 
     for variable in cfg.variables:
         # Check if the input file exists
-        input_file = os.path.join(cfg.data_dir, variable)
+        input_file = os.path.join(cfg.data_dir, variable.name)
         if not os.path.isfile(input_file):
-            logging.warning(f"Input file {input_file} not found. Skipping this variable.")
+            logging.warning(
+                f"Input file {input_file} not found. Skipping this variable."
+            )
             continue
 
-        logging.info(variable.split('_')[0].upper())
-        intermediate_file = os.path.join(cfg.output_dir, f'{variable.split(".")[0]}_sellonlatbox_cropped.nc')
-        output_file = os.path.join(cfg.output_dir, f'{variable.split(".")[0]}_time_sliced_cropped.nc')
+        logging.info(variable.name.split("_")[0].upper())
+        intermediate_file = os.path.join(
+            cfg.output_dir, f'{variable.name.split(".")[0]}_sellonlatbox_cropped.nc'
+        )
+        output_file = os.path.join(
+            cfg.output_dir, f'{variable.name.split(".")[0]}_time_sliced_cropped.nc'
+        )
 
         # Build the CDO command for sellonlatbox
         sellonlatbox_command = f"cdo -sellonlatbox,{cfg.lon_min},{cfg.lon_max},{cfg.lat_min},{cfg.lat_max} {input_file} {intermediate_file}"
@@ -35,7 +44,7 @@ def main(cfg):
             logging.info("sellonlatbox completed!")
 
             # Build the CDO command for time slicing
-            slice_time_command = f"cdo seldate,{cfg.start_date},{cfg.end_date} {intermediate_file} {output_file}"
+            slice_time_command = f"cdo seldate,{variable.start_date},{variable.end_date} {intermediate_file} {output_file}"
 
             logging.info(f"Executing time slicing command: {slice_time_command}")
 
@@ -49,6 +58,7 @@ def main(cfg):
 
         except subprocess.CalledProcessError as e:
             logging.error(f"Error: {e}")
+
 
 if __name__ == "__main__":
     main()
---
+++ tools/cdo_scripts/single_file_to_batches.py
@@ -0,0 +1,85 @@
+from dask.distributed import Client
+import torch
+from datetime import timedelta
+from timeit import default_timer as timer
+import numpy as np
+import hydra
+import logging
+import os
+import glob
+from multiprocessing import Pool
+from joblib import Parallel, delayed
+from parallelbar import progress_imap, progress_starmap, progress_imapu
+
+from multiprocessing import set_start_method
+from multiprocessing import get_context
+
+torch.manual_seed(0)
+
+def parallel_loop(sub_i, path, output_path):
+    sub_path = [f"{path}_{j}.pt" for j in sub_i]
+    stack = [torch.load(sub) for sub in sub_path]
+    x = torch.stack(stack, dim=0)
+    assert not torch.isnan(x).any(), f"NaNs found in {i}"
+    torch.save(x, output_path)
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def main(cfg) -> None:
+    # Define for loop that iterates over sets, resolutions, and variables
+    # and saves each time step as a torch tensor to write to a pytorch file
+    # format.
+
+    indices = torch.randperm(len(glob.glob(f"{cfg.vars['uas'].output_path}/train/uas/lr/*.pt")))
+    indices = torch.split(
+        indices, cfg.loader.batch_size, dim=0
+    )
+
+    for res in ["lr", "hr"]:
+        start = timer()
+        for s in ["train"]:
+            logging.info(f"Loading {s} {res} dataset...")
+            for var in cfg.vars:
+                output_path = cfg.vars[var].output_path
+                if not os.path.exists(f"{output_path}/batched/{s}/{var}/{res}"):
+                    logging.info(
+                        f"Creating directory: {output_path}/batched/{s}/{var}/{res}"
+                    )
+                    os.makedirs(f"{output_path}/batched/{s}/{var}/{res}")
+
+                partial_paths = [
+                    f"{output_path}/{s}/{var}/{res}/{var}"
+                    for i in range(len(indices))
+                ]
+
+                output_path = [
+                    f"{output_path}/batched/{s}/{var}/{res}/{var}_{i}.pt"
+                    for i in range(len(indices))
+                ]
+
+
+
+                pool_tuple = zip(
+                    indices,
+                    partial_paths,
+                    output_path
+                )
+
+                # if __name__ == "__main__":
+                with get_context("spawn").Pool() as pool:
+                    # progress_starmap(
+                    pool.starmap(
+                        parallel_loop,
+                        pool_tuple,
+                        # total=len(indices),
+                        # n_cpu=16,
+                    )
+                logging.info(f"Completed {s} {res} {var} batching")
+
+        end = timer()
+        logging.info(f"Finished {res} dataset in {timedelta(seconds=end-start)}")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+    # with Client(n_workers=8, threads_per_worker=2, processes=False, dashboard_address=8787, memory_limit='4GB'):
+    main()
